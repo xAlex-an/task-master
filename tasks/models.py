7f67006308c1f6b1d1bacd66e695f9650637615c
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-18 15:25:23 +0100

    Add initial migration and models for Category and Task in tasks app

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
+
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=100, unique=True)
+
+	def __str__(self):
+		return self.name
+
+class Task(models.Model):
+	title = models.CharField(max_length=200)
+	due_date = models.DateField()
+	completed = models.BooleanField(default=False)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='tasks')
+
+	def __str__(self):
+		return self.title
