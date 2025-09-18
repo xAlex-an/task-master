b752d27bbd4c40fc14ea6d2ab966d004474e2685
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-18 18:00:58 +0100

    Implement home view to display uncompleted and completed tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,15 @@
+
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
+def home(request):
+	"""A view to display the home page with tasks."""
+
+	uncompleted_tasks = Task.objects.filter(completed=False).order_by('due_date')
+	completed_tasks = Task.objects.filter(completed=True).order_by('due_date')
+	context = {
+		'uncompleted_tasks': uncompleted_tasks,
+		'completed_tasks': completed_tasks,
+	}
+	return render(request, 'tasks/index.html', context)
