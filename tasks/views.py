291f90a78a09f6c5c96398aba318bf63fabbad4a
Aleksandra Proc <aleksandraproc@yahoo.co.uk>
2025-09-18 19:10:44 +0100

    Refactor home view to include task form handling and improve code structure

--- tasks/views.py
+++ tasks/views.py
@@ -1,15 +1,25 @@
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def home(request):
-	"""A view to display the home page with tasks."""
+  """A view to display the home page with tasks."""
 
-	uncompleted_tasks = Task.objects.filter(completed=False).order_by('due_date')
-	completed_tasks = Task.objects.filter(completed=True).order_by('due_date')
-	context = {
-		'uncompleted_tasks': uncompleted_tasks,
-		'completed_tasks': completed_tasks,
-	}
-	return render(request, 'tasks/index.html', context)
+  uncompleted_tasks = Task.objects.filter(completed=False).order_by('due_date')
+  completed_tasks = Task.objects.filter(completed=True).order_by('due_date')
+
+  if request.method == 'POST':
+    form = TaskForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+  else:
+    form = TaskForm()
+
+  context = {
+    'uncompleted_tasks': uncompleted_tasks,
+    'completed_tasks': completed_tasks,
+    'form': form,
+  }
+  return render(request, 'tasks/index.html', context)
